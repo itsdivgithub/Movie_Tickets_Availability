56d6e18c2bb2dd98bdd1b9059c8480cebf254124
itsdivgithub <10x.divminds@gmail.com>
2022-10-20 01:33:16 +0530

    Update bookmyshow.py

--- bookmyshow.py
+++ bookmyshow.py
@@ -36,4 +36,21 @@ def create_popup():
 
     return
 
-def parse_and_check_tickets(url_openned, features="html5lib"):
+def parse_and_check_tickets(url_openned):
+
+    url_soup= BeautifulSoup(url_openned , features="html5lib")
+    a_class = url_soup.find_all("a", {"class": "_venue-name"})
+    a_class_str = str(a_class)
+    a_class_soup= Beautiful_soup(a_class_str, features="html5lib")
+    strong= a_class_soup.find_all("strong")
+    strong_str= str(strong)
+
+    for element in strong:
+        result= re.search(rf'{cinema}', str(element))
+        if result is None:
+            continue
+        else:
+            create_popup()
+            exit()
+    display.display("Tickets were not available at :" + str(datetime.datetime.now()))
+    return
