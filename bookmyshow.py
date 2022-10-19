e756478fdbcb1a8fc46e82b23e0335d5d44e49c9
itsdivgithub <10x.divminds@gmail.com>
2022-10-20 01:13:22 +0530

    Update bookmyshow.py

--- bookmyshow.py
+++ bookmyshow.py
@@ -8,3 +8,32 @@ import webbrowser
 import datetime
 from IPython import display
 from bs4 import BeautifulSoup
+
+url='https://in.bookmyshow.com/buytickets/act-1978-bengaluru/movie-bang-ET00300389-MT/20201128'
+cinema='PVR: South X Mall, Kanpur'
+
+def open_url():
+    chrome_path ='open -a /Applications/Google\ Chrome.app %s'
+    webbrowser.get(chrome_path).open(url)
+    return
+
+
+def create_popup():
+    root= tkinter.Tk()
+    text= 'Theater is now Open!'
+    label= tkinter.Label(root, text=text)
+    label.pack()
+    test= tkinter.Button(root, text="Click Me!", command=open_url)
+    test.pack()
+    root.test =test
+    quit= tkinter.Button(root, text="Quit", command= root.destroy)
+    quit.pack()
+
+    root.iconify()
+    root.update()
+    root.deiconify()
+    root.mainloop()
+
+    return
+
+    
