4178438cd01806f126d555d9d318aa3e5fa2fde0
itsdivgithub <10x.divminds@gmail.com>
2022-10-20 00:58:40 +0530

    Create bookmyshow.py

---
+++ bookmyshow.py
@@ -0,0 +1,10 @@
+import sys
+import os 
+import time
+import re
+import urllib.request as urllib2
+import tkinter
+import webbrowser
+import datetime
+from IPython import display
+from bs4 import BeautifulSoup
